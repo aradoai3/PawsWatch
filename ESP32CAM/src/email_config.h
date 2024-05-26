93b058103e5f6c41c03806498d18906ba5c75a42
aradoai3 <67860499+aradoai3@users.noreply.github.com>
2024-05-27 01:36:08 +0300

    Update email_config.h

--- ESP32CAM/src/email_config.h
+++ ESP32CAM/src/email_config.h
@@ -10,8 +10,8 @@ void sendEmail(String message);
 
 #define SMTP_HOST "smtp.gmail.com"
 #define SMTP_PORT 465
-#define AUTHOR_EMAIL "your_email@gmail.com"
-#define AUTHOR_PASSWORD "your_email_password"
-#define RECIPIENT_EMAIL "recipient_email@example.com"
+#define AUTHOR_EMAIL "ioanaaradoaie06@gmail.com"
+#define AUTHOR_PASSWORD "PAROLAMEA"
+#define RECIPIENT_EMAIL "ioana_aradoaie@yahoo.com"
 
 #endif
