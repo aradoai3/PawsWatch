03ac36108c231399eeec7bff9db9425e57f0bae2
aradoai3 <67860499+aradoai3@users.noreply.github.com>
2024-05-27 01:37:38 +0300

    Update email_config.cpp

--- ESP32CAM/src/email_config.cpp
+++ ESP32CAM/src/email_config.cpp
@@ -4,14 +4,23 @@
 SMTPSession smtp;
 
 void initEmail() {
-    // Configurații suplimentare, dacă este necesar
+    // Setare login pentru SMTP
+    smtp.setLogin(SMTP_HOST, SMTP_PORT, AUTHOR_EMAIL, AUTHOR_PASSWORD);
+
+    // Configurații suplimentare SMTP
+    smtp.setClientSecure();
+    smtp.setSTARTTLS(true);
+    
+    // Setări pentru email
+    smtp.setSender("ESP32-CAM", AUTHOR_EMAIL);
+    smtp.setPriority("High");
 }
 
 void sendEmail(String message) {
     SMTP_Message email;
     email.sender.name = "ESP32-CAM";
     email.sender.email = AUTHOR_EMAIL;
-    email.subject = "Dog Water Consumption Alert";
+    email.subject = "Alerta: Cainele a baut apa";
     email.message.text = message.c_str();
     email.addRecipient("Recipient", RECIPIENT_EMAIL);
 
